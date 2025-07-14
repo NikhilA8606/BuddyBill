125d7623a288c452d813475e6b3b14d3cfed05e9
nikhila8606 <nikhila8606@gmail.com>
2025-07-14 23:39:41 +0530

    Additionally added models and cleanup files

--- buddybill/models.py
+++ buddybill/models.py
@@ -4,9 +4,15 @@ from django.db import models
 from users.models import User
 from utils.models import BaseModel
 
+
 class TopicTag(BaseModel):
     name = models.CharField(max_length=255)
-    created_by = models.ForeignKey(User,on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+class TransactionCategory(models.TextChoices):
+    EXPENSE = "EXPENSE"
+    SETTLEMENT = "SETTLEMENT"
 
 
 class Transaction(BaseModel):
@@ -14,8 +20,31 @@ class Transaction(BaseModel):
         max_length=255,
     )
     total_amount = models.IntegerField(validators=[MinValueValidator(0)])
-    topic = models.ForeignKey(TopicTag,on_delete=models.PROTECT,null=True)
-    category = models.CharField(choices=["expense","settlement"],max_length=255)
+    topic = models.ForeignKey(TopicTag, on_delete=models.PROTECT, null=True)
+    category = models.CharField(
+        choices=TransactionCategory,
+        max_length=15,
+        default=TransactionCategory.EXPENSE,
+    )
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
 
 
+class TransactionEntry(BaseModel):
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    amount = models.IntegerField(validators=[MinValueValidator(0)])
+    buddy = models.ForeignKey(User, on_delete=models.PROTECT)
 
+
+class BuddyGroup(BaseModel):
+    name = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+class BuddyGroupUser(BaseModel):
+    buddy_group = models.ForeignKey(BuddyGroup, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+class UserFriend(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="friends")
+    friend = models.ForeignKey(User, on_delete=models.PROTECT, related_name="+")
--- buddybill/urls.py
+++ buddybill/urls.py
@@ -1,8 +1,3 @@
 from rest_framework import routers
 
-from .views import (
-    GroupViewSet,
-)
-
 buddybill_router = routers.SimpleRouter()
-buddybill_router.register(r"buddybill/group", GroupViewSet)
